5f80289955c8e5351d325ff40264322c02da41aa
zhufengnodejs <zhufengnodejs@126.com>
2016-08-04 13:02:08 +0800

    2

--- comment/index.js
+++ comment/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 require('bootstrap/dist/css/bootstrap.css');
 //导入容器组件
 import {CommentBox} from './containers';
+
 //把留言板组件渲染到页面的ID为app的div内部
 ReactDOM.render(
     <CommentBox/>,
